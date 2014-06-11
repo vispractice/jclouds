c8eded19fc69c73ae55fe7f42608e440e33429ea
gaozheng <gaozheng.cn@gmail.com>
2014-06-11 16:25:18 +0800

    fix listnetwork offering options fields

--- apis/cloudstack/src/main/java/org/jclouds/cloudstack/options/ListNetworkOfferingsOptions.java
+++ apis/cloudstack/src/main/java/org/jclouds/cloudstack/options/ListNetworkOfferingsOptions.java
@@ -16,6 +16,8 @@
  */
 package org.jclouds.cloudstack.options;
 
+import org.jclouds.cloudstack.domain.GuestIPType;
+import org.jclouds.cloudstack.domain.NetworkOffering;
 import org.jclouds.cloudstack.domain.NetworkOfferingAvailabilityType;
 import org.jclouds.cloudstack.domain.TrafficType;
 import org.jclouds.http.options.BaseHttpRequestOptions;
@@ -115,7 +117,73 @@ public class ListNetworkOfferingsOptions extends BaseHttpRequestOptions {
       this.queryParameters.replaceValues("traffictype", ImmutableSet.of(trafficType.toString()));
       return this;
    }
+   
+   public ListNetworkOfferingsOptions forVpc(boolean forVpc) {
+       this.queryParameters.replaceValues("forvpc", ImmutableSet.of(forVpc + ""));
+       return this;
+    }
+   
+   public ListNetworkOfferingsOptions guestIpType(GuestIPType guestIpType) {
+       this.queryParameters.replaceValues("guestiptype", ImmutableSet.of(guestIpType.toString()));
+       return this;
+    }
+   
+   public ListNetworkOfferingsOptions isTagged(boolean isTagged) {
+       this.queryParameters.replaceValues("istagged", ImmutableSet.of(isTagged + ""));
+       return this;
+    }
+   
+   public ListNetworkOfferingsOptions keyword(String keyword) {
+       this.queryParameters.replaceValues("keyword", ImmutableSet.of(keyword));
+       return this;
+    }
+   
+   public ListNetworkOfferingsOptions networkId(String networkId) {
+       this.queryParameters.replaceValues("networkid", ImmutableSet.of(networkId));
+       return this;
+    }
 
+   /**
+    * @param page
+    */
+   public ListNetworkOfferingsOptions page(long page) {
+      this.queryParameters.replaceValues("page", ImmutableSet.of(page + ""));
+      return this;
+   }
+
+   /**
+    * @param pageSize the page size
+    */
+   public ListNetworkOfferingsOptions pageSize(long pageSize) {
+      this.queryParameters.replaceValues("pagesize", ImmutableSet.of(pageSize + ""));
+      return this;
+   }
+   
+   public ListNetworkOfferingsOptions sourceNatSupported(boolean sourceNatSupported) {
+       this.queryParameters.replaceValues("sourcenatsupported", ImmutableSet.of(sourceNatSupported + ""));
+       return this;
+    }
+   
+   public ListNetworkOfferingsOptions specifyIpRanges(boolean specifyIpRanges) {
+       this.queryParameters.replaceValues("specifyipranges", ImmutableSet.of(specifyIpRanges + ""));
+       return this;
+    }
+   
+   public ListNetworkOfferingsOptions state(NetworkOffering.State state) {
+       this.queryParameters.replaceValues("state", ImmutableSet.of(state.toString()));
+       return this;
+    }
+   
+   public ListNetworkOfferingsOptions supportedServices(String supportedServices) {
+       this.queryParameters.replaceValues("supportedservices", ImmutableSet.of(supportedServices));
+       return this;
+    }
+   
+   public ListNetworkOfferingsOptions tags(String tags) {
+       this.queryParameters.replaceValues("tags", ImmutableSet.of(tags));
+       return this;
+    }
+   
    public static class Builder {
       /**
        * @see ListNetworkOfferingsOptions#specifyVLAN
@@ -188,5 +256,65 @@ public class ListNetworkOfferingsOptions extends BaseHttpRequestOptions {
          ListNetworkOfferingsOptions options = new ListNetworkOfferingsOptions();
          return options.trafficType(trafficType);
       }
+      
+      public static ListNetworkOfferingsOptions forVpc(boolean forVpc) {
+          ListNetworkOfferingsOptions options = new ListNetworkOfferingsOptions();
+          return options.forVpc(forVpc);
+       }
+      
+      public static ListNetworkOfferingsOptions guestIpType(GuestIPType guestIpType) {
+          ListNetworkOfferingsOptions options = new ListNetworkOfferingsOptions();
+          return options.guestIpType(guestIpType);
+       }
+      
+      public static ListNetworkOfferingsOptions isTagged(boolean isTagged) {
+          ListNetworkOfferingsOptions options = new ListNetworkOfferingsOptions();
+          return options.isTagged(isTagged);
+       }
+      
+      public static ListNetworkOfferingsOptions keyword(String keyword) {
+          ListNetworkOfferingsOptions options = new ListNetworkOfferingsOptions();
+          return options.keyword(keyword);
+       }
+      
+      public static ListNetworkOfferingsOptions networkId(String networkId) {
+          ListNetworkOfferingsOptions options = new ListNetworkOfferingsOptions();
+          return options.networkId(networkId);
+       }
+      
+      public static ListNetworkOfferingsOptions page(long page) {
+          ListNetworkOfferingsOptions options = new ListNetworkOfferingsOptions();
+          return options.page(page);
+       }
+      
+      public static ListNetworkOfferingsOptions pageSize(long pageSize) {
+          ListNetworkOfferingsOptions options = new ListNetworkOfferingsOptions();
+          return options.pageSize(pageSize);
+       }
+      
+      public static ListNetworkOfferingsOptions sourceNatSupported(boolean sourceNatSupported) {
+          ListNetworkOfferingsOptions options = new ListNetworkOfferingsOptions();
+          return options.sourceNatSupported(sourceNatSupported);
+       }
+      
+      public static ListNetworkOfferingsOptions specifyIpRanges(boolean specifyIpRanges) {
+          ListNetworkOfferingsOptions options = new ListNetworkOfferingsOptions();
+          return options.specifyIpRanges(specifyIpRanges);
+       }
+      
+      public static ListNetworkOfferingsOptions state(NetworkOffering.State state) {
+          ListNetworkOfferingsOptions options = new ListNetworkOfferingsOptions();
+          return options.state(state);
+       }
+      
+      public static ListNetworkOfferingsOptions supportedServices(String supportedServices) {
+          ListNetworkOfferingsOptions options = new ListNetworkOfferingsOptions();
+          return options.supportedServices(supportedServices);
+       }
+      
+      public static ListNetworkOfferingsOptions tags(String tags) {
+          ListNetworkOfferingsOptions options = new ListNetworkOfferingsOptions();
+          return options.tags(tags);
+       }
    }
 }
